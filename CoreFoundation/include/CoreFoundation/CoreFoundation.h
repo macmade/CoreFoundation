d67418b56899d61176069900b2cd5457e4d530d8
macmade <macmade@xs-labs.com>
2016-10-06 12:11:15 +0200

    Initial project import...

---
+++ CoreFoundation/include/CoreFoundation/CoreFoundation.h
@@ -0,0 +1,85 @@
+/*******************************************************************************
+ * The MIT License (MIT)
+ * 
+ * Copyright (c) 2016 Jean-David Gadina - www-xs-labs.com
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ ******************************************************************************/
+
+/*!
+ * @header      CoreFoundation.h
+ * @copyright   (c) 2016, Jean-David Gadina - www.xs-labs.com
+ */
+
+#ifndef CORE_FOUNDATION_H
+#define CORE_FOUNDATION_H
+
+#include <CoreFoundation/CFAllocator.h>
+#include <CoreFoundation/CFArray.h>
+#include <CoreFoundation/CFAttributedString.h>
+#include <CoreFoundation/CFAvailability.h>
+#include <CoreFoundation/CFBag.h>
+#include <CoreFoundation/CFBase.h>
+#include <CoreFoundation/CFBinaryHeap.h>
+#include <CoreFoundation/CFBitVector.h>
+#include <CoreFoundation/CFBoolean.h>
+#include <CoreFoundation/CFBundle.h>
+#include <CoreFoundation/CFByteOrder.h>
+#include <CoreFoundation/CFCalendar.h>
+#include <CoreFoundation/CFCharacterSet.h>
+#include <CoreFoundation/CFData.h>
+#include <CoreFoundation/CFDate.h>
+#include <CoreFoundation/CFDateFormatter.h>
+#include <CoreFoundation/CFDictionary.h>
+#include <CoreFoundation/CFError.h>
+#include <CoreFoundation/CFFileDescriptor.h>
+#include <CoreFoundation/CFLocale.h>
+#include <CoreFoundation/CFMachPort.h>
+#include <CoreFoundation/CFMessagePort.h>
+#include <CoreFoundation/CFNotificationCenter.h>
+#include <CoreFoundation/CFNull.h>
+#include <CoreFoundation/CFNumber.h>
+#include <CoreFoundation/CFNumberFormatter.h>
+#include <CoreFoundation/CFPlugIn.h>
+#include <CoreFoundation/CFPlugInInstance.h>
+#include <CoreFoundation/CFPreferences.h>
+#include <CoreFoundation/CFPropertyList.h>
+#include <CoreFoundation/CFReadStream.h>
+#include <CoreFoundation/CFRunLoop.h>
+#include <CoreFoundation/CFRunLoopObserver.h>
+#include <CoreFoundation/CFRunLoopSource.h>
+#include <CoreFoundation/CFRunLoopTimer.h>
+#include <CoreFoundation/CFSet.h>
+#include <CoreFoundation/CFSocket.h>
+#include <CoreFoundation/CFStream.h>
+#include <CoreFoundation/CFString.h>
+#include <CoreFoundation/CFStringTokenizer.h>
+#include <CoreFoundation/CFTimeZone.h>
+#include <CoreFoundation/CFTree.h>
+#include <CoreFoundation/CFType.h>
+#include <CoreFoundation/CFURL.h>
+#include <CoreFoundation/CFUserNotification.h>
+#include <CoreFoundation/CFUtilities.h>
+#include <CoreFoundation/CFUUID.h>
+#include <CoreFoundation/CFWriteStream.h>
+#include <CoreFoundation/CFXMLNode.h>
+#include <CoreFoundation/CFXMLParser.h>
+#include <CoreFoundation/CFXMLTree.h>
+
+#endif /* CORE_FOUNDATION_H */
