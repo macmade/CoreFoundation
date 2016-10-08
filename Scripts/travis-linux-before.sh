7df0eec5ff1305adf8a1e9d97a25d1da6320b50a
macmade <macmade@xs-labs.com>
2016-10-08 12:03:04 +0200

    Travis...

---
+++ Scripts/travis-linux-before.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+. /etc/lsb-release
+
+declare -r MAJOR=$(echo "$DISTRIB_RELEASE" | perl -e "s/([0-9]+).*/\1/" -pi)
+
+if [ $MAJOR -lt 14 ]; then
+    
+    echo "Ubuntu 14 or greater is required"
+    exit 1
+
+fi
+
+sudo apt-get update
+
+sudo apt-get -y install make
+sudo apt-get -y install clang
+sudo apt-get -y install libc6
+sudo apt-get -y install libc6-dev
+sudo apt-get -y install libc++1
+sudo apt-get -y install libc++-dev
