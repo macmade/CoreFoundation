e352e878acb3f0f1d5a0ad649ccabfcccc251636
macmade <macmade@xs-labs.com>
2016-10-11 22:06:06 +0200

    CFNumber in progress...

--- CoreFoundation/source/CFNumber.c
+++ CoreFoundation/source/CFNumber.c
@@ -30,10 +30,18 @@
 #include <CoreFoundation/CoreFoundation.h>
 #include <CoreFoundation/__private/CFRuntime.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <math.h>
 
 struct CFNumber
 {
     CFRuntimeBase _base;
+    bool          _isFloat;
+    Float64       _doubleValue;
+    SInt64        _integerValue;
+    bool          _nan;
+    bool          _pinf;
+    bool          _ninf;
 };
 
 static CFTypeID CFNumberTypeID      = 0;
@@ -61,38 +69,161 @@ static void init( void )
     CFRuntimeInitStaticInstance( &CFNumberNaN,              CFNumberTypeID );
     CFRuntimeInitStaticInstance( &CFNumberNegativeInfinity, CFNumberTypeID );
     CFRuntimeInitStaticInstance( &CFNumberPositiveInfinity, CFNumberTypeID );
+    
+    CFNumberNaN._isFloat              = true;
+    CFNumberNegativeInfinity._isFloat = true;
+    CFNumberPositiveInfinity._isFloat = true;
+    
+    CFNumberNaN._nan               = true;
+    CFNumberNegativeInfinity._pinf = true;
+    CFNumberPositiveInfinity._ninf = true;
 }
 
 CFTypeID CFNumberGetTypeID( void )
 {
-    return 0;
+    return CFNumberTypeID;
 }
 
 CFNumberRef CFNumberCreate( CFAllocatorRef allocator, CFNumberType type, const void * valuePtr )
 {
-    ( void )allocator;
-    ( void )type;
-    ( void )valuePtr;
+    struct CFNumber * o;
     
-    return NULL;
+    if( valuePtr == NULL )
+    {
+        return NULL;
+    }
+    
+    o = ( struct CFNumber * )CFRuntimeCreateInstance( allocator, CFNumberTypeID );
+    
+    if( type == kCFNumberSInt8Type )
+    {
+        o->_isFloat      = false;
+        o->_integerValue = ( SInt64 )*( ( const SInt8 * )valuePtr );
+    }
+    else if( type == kCFNumberSInt16Type )
+    {
+        o->_isFloat      = false;
+        o->_integerValue = ( SInt64 )*( ( const SInt16 * )valuePtr );
+    }
+    else if( type == kCFNumberSInt32Type )
+    {
+        o->_isFloat      = false;
+        o->_integerValue = ( SInt64 )*( ( const SInt32 * )valuePtr );
+    }
+    else if( type == kCFNumberSInt64Type )
+    {
+        o->_isFloat      = false;
+        o->_integerValue = ( SInt64 )*( ( const SInt64 * )valuePtr );
+    }
+    else if( type == kCFNumberFloat32Type )
+    {
+        o->_isFloat      = true;
+        o->_doubleValue  = ( Float64 )*( ( const Float32 * )valuePtr );
+    }
+    else if( type == kCFNumberFloat64Type )
+    {
+        o->_isFloat      = true;
+        o->_doubleValue  = ( Float64 )*( ( const Float64 * )valuePtr );
+    }
+    else if( type == kCFNumberCharType )
+    {
+        o->_isFloat      = false;
+        o->_integerValue = ( SInt64 )*( ( const char * )valuePtr );
+    }
+    else if( type == kCFNumberShortType )
+    {
+        o->_isFloat      = false;
+        o->_integerValue = ( SInt64 )*( ( const short * )valuePtr );
+    }
+    else if( type == kCFNumberIntType )
+    {
+        o->_isFloat      = false;
+        o->_integerValue = ( SInt64 )*( ( const int * )valuePtr );
+    }
+    else if( type == kCFNumberLongType )
+    {
+        o->_isFloat      = false;
+        o->_integerValue = ( SInt64 )*( ( const long * )valuePtr );
+    }
+    else if( type == kCFNumberLongLongType )
+    {
+        o->_isFloat      = false;
+        o->_integerValue = ( SInt64 )*( ( const long long * )valuePtr );
+    }
+    else if( type == kCFNumberFloatType )
+    {
+        o->_isFloat      = true;
+        o->_doubleValue  = ( double )*( ( const float * )valuePtr );
+    }
+    else if( type == kCFNumberDoubleType )
+    {
+        o->_isFloat      = true;
+        o->_doubleValue  = ( SInt64 )*( ( const double * )valuePtr );
+    }
+    else if( type == kCFNumberCFIndexType )
+    {
+        o->_isFloat      = false;
+        o->_integerValue = ( SInt64 )*( ( const CFIndex * )valuePtr );
+    }
+    else if( type == kCFNumberNSIntegerType )
+    {
+        #if defined( __LP64__ ) && __LP64__
+        
+        o->_isFloat      = false;
+        o->_integerValue = ( SInt64 )*( ( const long * )valuePtr );
+        
+        #else
+        
+        o->_isFloat      = false;
+        o->_integerValue = ( SInt64 )*( ( const int * )valuePtr );
+        
+        #endif
+    }
+    else if( type == kCFNumberCGFloatType || type == kCFNumberMaxType )
+    {
+        #if defined( __LP64__ ) && __LP64__
+        
+        o->_isFloat      = true;
+        o->_doubleValue  = ( Float64 )*( ( const double * )valuePtr );
+        
+        #else
+        
+        o->_isFloat      = true;
+        o->_doubleValue  = ( Float64 )*( ( const float * )valuePtr );
+        
+        #endif
+    }
+    
+    return o;
 }
 
 CFIndex CFNumberGetByteSize( CFNumberRef number )
 {
-    ( void )number;
+    if( number == NULL )
+    {
+        return 0;
+    }
     
-    return 0;
+    return ( number->_isFloat ) ? sizeof( double ) : sizeof( uint64_t );
 }
 
 CFNumberType CFNumberGetType( CFNumberRef number )
 {
-    ( void )number;
+    if( number == NULL )
+    {
+        return kCFNumberSInt8Type;
+    }
     
-    return kCFNumberSInt8Type;
+    return ( number->_isFloat ) ? kCFNumberFloat64Type : kCFNumberSInt64Type;
 }
 
 Boolean CFNumberGetValue( CFNumberRef number, CFNumberType type, void * valuePtr )
 {
+    if( valuePtr == NULL )
+    {
+        return false;
+    }
+    
     ( void )number;
     ( void )type;
     ( void )valuePtr;
@@ -102,9 +233,12 @@ Boolean CFNumberGetValue( CFNumberRef number, CFNumberType type, void * valuePtr
 
 Boolean CFNumberIsFloatType( CFNumberRef number )
 {
-    ( void )number;
+    if( number == NULL )
+    {
+        return false;
+    }
     
-    return false;
+    return number->_isFloat;
 }
 
 CFComparisonResult CFNumberCompare( CFNumberRef number, CFNumberRef otherNumber, void * context )
