f3ee6dbf24d9c2d327ae3065c8b56b5b492b35c6
macmade <macmade@xs-labs.com>
2016-10-11 20:26:46 +0200

    Runtime functions...

--- CoreFoundation/include/CoreFoundation/__private/CFRuntime.h
+++ CoreFoundation/include/CoreFoundation/__private/CFRuntime.h
@@ -31,6 +31,7 @@
 #define CORE_FOUNDATION_CF___PRIVATE_RUNTIME_H
 
 #include <CoreFoundation/CFBase.h>
+#include <CoreFoundation/CFAllocator.h>
 #include <stdint.h>
 #include <stdlib.h>
 
@@ -48,8 +49,9 @@ CF_EXTERN_C_BEGIN
  */
 typedef struct
 {
-    CFTypeID isa;
-    uint64_t rc;
+    CFTypeID         isa;
+    volatile CFIndex rc;
+    CFAllocatorRef   allocator;
 }
 CFRuntimeBase;
 
@@ -66,8 +68,8 @@ typedef struct
     const char * name;
     size_t       size;
     
-    void ( * constructor )( void * obj );
-    void ( * destructor )( void * obj );
+    void ( * constructor )( CFTypeRef obj );
+    void ( * destructor )( CFTypeRef obj );
 }
 CFRuntimeClass;
 
@@ -80,12 +82,38 @@ CFRuntimeClass;
 CF_EXPORT CFTypeID CFRuntimeRegisterClass( const CFRuntimeClass * cls );
 
 /*!
+ * @function    CFRuntimeCreateInstance
+ * @abstract    Creates a new instance of a CoreFoundation class.
+ * @param       allocator   The allocator to use
+ * @param       typeID      The type ID of the class
+ * @result      The new instance
+ */
+CF_EXPORT CFTypeRef CFRuntimeCreateInstance( CFAllocatorRef allocator, CFTypeID typeID );
+
+/*!
+ * @function    CFRuntimeGetInstanceSize
+ */
+CF_EXPORT CFIndex CFRuntimeGetInstanceSize( CFTypeID typeID );
+
+/*!
+ * @function    CFRuntimeInitInstance
+ */
+CF_EXPORT void CFRuntimeInitInstance( void * memory, CFTypeID typeID, CFAllocatorRef allocator );
+
+/*!
  * @function    CFRuntimeInitStaticInstance
  * @abstract    Initializes a static instance of a CoreFoundation class.
  * @param       memory      The static class instance
  * @param       typeID      The ID of the class
  */
 CF_EXPORT void CFRuntimeInitStaticInstance( void * memory, CFTypeID typeID );
+
+/*!
+ * @function    CFRuntimeDeleteInstance
+ * @abstract    Deletes an instance of a CoreFoundation class.
+ * @param       obj     The class instance to delete
+ */
+CF_EXPORT void CFRuntimeDeleteInstance( CFTypeRef obj );
 
 /*!
  * @function    CFAbortWithError
--- CoreFoundation/source/__private/CFRuntime.c
+++ CoreFoundation/source/__private/CFRuntime.c
@@ -31,25 +31,186 @@
 #include <CoreFoundation/__private/CFAtomic.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+struct CFRuntimeClassList
+{
+    CFRuntimeClass                       cls;
+    CFTypeID                             typeID;
+    struct CFRuntimeClassList * volatile next;
+};
+
+CFIndex CFRuntimeClassCount                           = 0;
+struct CFRuntimeClassList * volatile CFRuntimeClasses = NULL;
 
 CFTypeID CFRuntimeRegisterClass( const CFRuntimeClass * cls )
 {
+    struct CFRuntimeClassList * new;
+    struct CFRuntimeClassList * list;
+    CFTypeID                    typeID;
+    
     if( cls == NULL )
     {
         return 0;
     }
     
+    new = calloc( sizeof( struct CFRuntimeClassList ), 1 );
+    
+    if( new == NULL )
+    {
+        CFAbortWithError( "Out of memory" );
+        
+        return 0;
+    }
+    
+    typeID      = ( CFTypeID )CFAtomicIncrement( &CFRuntimeClassCount );
+    new->cls    = *( cls );
+    new->typeID = typeID;
+    
+    if( CFAtomicCompareAndSwapPointer( NULL, new, ( void * volatile * )&CFRuntimeClasses ) )
+    {
+        return typeID;
+    }
+    
+    list = CFRuntimeClasses;
+    
+    while( list )
+    {
+        if( CFAtomicCompareAndSwapPointer( NULL, new, ( void * volatile * )&( list->next ) ) )
+        {
+            return typeID;
+        }
+        
+        list = list->next;
+    }
+    
     return 0;
 }
 
-void CFRuntimeInitStaticInstance( void * memory, CFTypeID type )
+CFTypeRef CFRuntimeCreateInstance( CFAllocatorRef allocator, CFTypeID typeID )
 {
+    void * obj;
+    
+    if( allocator == NULL )
+    {
+        allocator = CFAllocatorGetDefault();
+    }
+    
+    obj = CFAllocatorAllocate( allocator, CFRuntimeGetInstanceSize( typeID ), 0 );
+    
+    CFRuntimeInitInstance( obj, typeID, allocator );
+    
+    return obj;
+}
+
+CFIndex CFRuntimeGetInstanceSize( CFTypeID typeID )
+{
+    struct CFRuntimeClassList * list;
+    
+    list = CFRuntimeClasses;
+    
+    while( list )
+    {
+        if( list->typeID != typeID )
+        {
+            list = list->next;
+            
+            continue;
+        }
+        
+        return ( CFIndex )( list->cls.size );
+    }
+    
+    return 0;
+}
+
+void CFRuntimeInitInstance( void * memory, CFTypeID typeID, CFAllocatorRef allocator )
+{
+    struct CFRuntimeClassList * list;
+    CFRuntimeBase             * base;
+    
     if( memory == NULL )
     {
         return;
     }
     
-    ( void )type;
+    list = CFRuntimeClasses;
+    
+    while( list )
+    {
+        if( list->typeID != typeID )
+        {
+            list = list->next;
+            
+            continue;
+        }
+        
+        memset( memory, 0, list->cls.size );
+        
+        base            = ( CFRuntimeBase * )memory;
+        base->isa       = typeID;
+        base->rc        = 1;
+        base->allocator = ( allocator ) ? CFRetain( allocator ) : NULL;
+        
+        if( list->cls.constructor )
+        {
+            list->cls.constructor( memory );
+        }
+        
+        break;
+    }
+}
+
+void CFRuntimeInitStaticInstance( void * memory, CFTypeID typeID )
+{
+    CFRuntimeBase * base;
+    
+    if( memory == NULL )
+    {
+        return;
+    }
+    
+    CFRuntimeInitInstance( memory, typeID, NULL );
+    
+    base     = ( CFRuntimeBase * )memory;
+    base->rc = -1;
+}
+
+void CFRuntimeDeleteInstance( CFTypeRef obj )
+{
+    struct CFRuntimeClassList * list;
+    CFRuntimeBase             * base;
+    
+    if( obj == NULL )
+    {
+        return;
+    }
+    
+    base = ( CFRuntimeBase * )obj;
+    list = CFRuntimeClasses;
+    
+    while( list )
+    {
+        if( list->typeID != base->isa )
+        {
+            list = list->next;
+            
+            continue;
+        }
+        
+        if( list->cls.destructor )
+        {
+            list->cls.destructor( obj );
+        }
+        
+        if( base->allocator )
+        {
+            CFAllocatorDeallocate( base->allocator, ( void * )obj );
+            CFRelease( base->allocator );
+        }
+        
+        break;
+    }
 }
 
 void CFAbortWithError( const char * error )
