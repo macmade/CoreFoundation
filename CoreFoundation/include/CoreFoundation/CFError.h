092e8161e8c3dc0c092364f09fee8b5854255b84
macmade <macmade@xs-labs.com>
2016-10-13 00:54:42 +0200

    Comments...

--- CoreFoundation/include/CoreFoundation/CFError.h
+++ CoreFoundation/include/CoreFoundation/CFError.h
@@ -25,6 +25,29 @@
 /*!
  * @header      CFError.h
  * @copyright   (c) 2016, Jean-David Gadina - www.xs-labs.com
+ * @discussion  A CFError object encapsulates more rich and extensible error
+ *              information than is possible using only an error code or error
+ *              string. The core attributes of a CFError object are an error
+ *              domain (represented by a string), a domain-specific error code,
+ *              and a "user info" dictionary containing application-specific
+ *              information. Errors are required to have a domain and an error
+ *              code within that domain. Several well-known domains are defined
+ *              corresponding to Mach, POSIX, and OSStatus errors.
+ *              The optional "user info" dictionary may provide additional
+ *              information that might be useful for the interpretation and
+ *              reporting of the error, including a human-readable description
+ *              for the error. The “user info” dictionary sometimes includes
+ *              another CFError object that represents an error in a subsystem
+ *              underlying the error represented by the containing CFError
+ *              object. This underlying error object may provide more specific
+ *              information about the cause of the error.
+ *              In general, a method should signal an error condition by
+ *              returning, for example, false or NULL rather than by the simple
+ *              presence of an error object. The method can then optionally
+ *              return an CFError object by reference, in order to further
+ *              describe the error.
+ *              CFError is toll-free bridged to NSError in the Foundation
+ *              framework.
  */
 
 #ifndef CORE_FOUNDATION_CF_ERROR_H
